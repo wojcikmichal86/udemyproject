05998756ed4c251d486f81b96f1b49ef82a06644
micwojci <micwojci@cisco.com>
2020-04-15 08:43:17 +0200

    Adding views

--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -18,6 +18,9 @@ class Post(models.Model):
     def approve_comments(self):
         return self.comments.filter(approved_comment=True)
 
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={'pk':self.pk})
+
     def __str__(self):
         return self.title
 
@@ -28,9 +31,12 @@ class Comment(models.Model):
     create_date = models.DateTimeField(default=timezone.now())
     approved_comment = models.BooleanField(default=False)
 
-    def approve(self):  
+    def approve(self):
         self.approved_comment = True
         self.save()
+
+    def get_absolute_url(self):
+        return reverse('post_list')
 
     def __str__(self):
         return self.text
